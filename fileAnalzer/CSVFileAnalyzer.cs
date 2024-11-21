9c619ad1c2940555f2fe4f04ab841f3bf833263f
amira maher <amira maher@DESKTOP-2I0OH7I>
2024-11-21 18:08:11 +0200

    EDITE

--- fileAnalzer/CSVFileAnalyzer.cs
+++ fileAnalzer/CSVFileAnalyzer.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 
 namespace fileAnalzer
 {
-    public class CSVFileAnalyzer : IFileAnalyzer
+    public class CSVFileAnalyzer : FileAnalyzer ,IFileAnalyzer
     {
         public void AnalyzeFile(FileInfo fileInfo)
         {
             string[] FileString = File.ReadAllLines(fileInfo.FullName);
+           var rowElements = FileString[0].Split(',');
             AnalysisResulte resulte = new AnalysisResulte();
+            resulte.FieldCount = rowElements.Length;
+
+            SetResulte(resulte);
         }
     }
 }
